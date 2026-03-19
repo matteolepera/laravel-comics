63070b507a8444f94d3d64f1bd1237fbd832d313
matteolpera <matteo.lepera02@gmail.com>
2026-03-19 12:36:56 +0100

    feat: aggiunto prefooter al layout.

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -13,6 +13,59 @@
     @include("partials.header")
     <main>
         @yield("main")
+
+
+        <!-- PREFOOTER -->
+        <section class="dc-prefooter">
+            <div class="dc-prefooter__inner">
+
+                <a href="#" class="dc-prefooter__item">
+                    <div class="dc-prefooter__icon-wrap">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="Digital Comics">
+                    </div>
+                    <span class="dc-prefooter__label">Digital Comics</span>
+                </a>
+
+                <div class="dc-prefooter__divider"></div>
+
+                <a href="#" class="dc-prefooter__item">
+                    <div class="dc-prefooter__icon-wrap">
+                        <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="DC Merchandise">
+                    </div>
+                    <span class="dc-prefooter__label">DC Merchandise</span>
+                </a>
+
+                <div class="dc-prefooter__divider"></div>
+
+                <a href="#" class="dc-prefooter__item">
+                    <div class="dc-prefooter__icon-wrap">
+                        <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="Subscription">
+                    </div>
+                    <span class="dc-prefooter__label">Subscription</span>
+                </a>
+
+                <div class="dc-prefooter__divider"></div>
+
+                <a href="#" class="dc-prefooter__item">
+                    <div class="dc-prefooter__icon-wrap">
+                        <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="Comic Shop Locator">
+                    </div>
+                    <span class="dc-prefooter__label">Comic Shop Locator</span>
+                </a>
+
+                <div class="dc-prefooter__divider"></div>
+
+                <a href="#" class="dc-prefooter__item dc-prefooter__item--visa">
+                    <div class="dc-prefooter__icon-wrap dc-prefooter__icon-wrap--visa">
+                        <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="DC Power Visa">
+                    </div>
+                    <span class="dc-prefooter__label">DC Power Visa</span>
+                </a>
+
+            </div>
+        </section>
+        <!-- PREFOOTER -->
+
     </main>
 
     @include("partials.footer")
